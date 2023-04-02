4b3f434924a2ca0eba77a06e83486efe55de70fa
Harjaspreet singh <singh.harjas2002@gmail.com>
2023-04-02 09:32:19 +0530

    ff

--- portapp/views.py
+++ portapp/views.py
@@ -16,15 +16,15 @@ import urllib
 @api_view()
 @permission_classes([AllowAny])
 def all(request):
-    mail=str(request.query_params['mail'])
-    apppass=str(request.query_params['apppass'])
-    name=str(request.query_params['name'])
-    sheet_id=str(request.query_params['sheet_id'])
-    sheet_name=str(request.query_params['sname'])
+    # mail=str(request.query_params['mail'])
+    # apppass=str(request.query_params['apppass'])
+    # name=str(request.query_params['name'])
+    # sheet_id=str(request.query_params['sheet_id'])
+    # sheet_name=str(request.query_params['sname'])
 
-    res=send_email(mail,apppass,name,sheet_id,sheet_name)
+    # res=send_email(mail,apppass,name,sheet_id,sheet_name)
     #file(sheet_id,sheet_name)
-    return Response({"RESPONSE":res})
+    return Response({"STATUS":"done"})
 
 
 
