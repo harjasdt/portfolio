434c26ca42d4c32031da78162f9536d0ff62f6c4
Harjaspreet singh <singh.harjas2002@gmail.com>
2023-10-25 17:04:04 +0530

    ss

--- portapp/models.py
+++ portapp/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class QUESTION(models.Model):
-    temp=models.DecimalField(blank=False,decimal_places=2,max_digits=8,default=0)
+    temp=models.CharField(max_length=900)
     time=models.IntegerField(blank=False,default=9)
