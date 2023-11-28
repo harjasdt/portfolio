cd17ce19c814535184bb00eaf7c027936d34adfc
Harjaspreet singh <singh.harjas2002@gmail.com>
2023-11-28 14:04:41 +0530

    ff

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['*',]
 # Application definition
 
 INSTALLED_APPS = [
-    'import_export',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
