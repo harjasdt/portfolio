9a2844ada291a993f455510d35be05035f360274
Harjaspreet singh <singh.harjas2002@gmail.com>
2023-11-28 14:00:59 +0530

    ff

--- portapp/admin.py
+++ portapp/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 from portapp.models import QUESTION,HISTORY,DOOR,DOORFAIL,ACTIVE,TEMP
+from import_export.admin import ExportActionMixin
+
 # Register your models here.
-admin.site.register(QUESTION)
+class BookAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ('temp','ph','mos','n','p','k','time')
+# Register your models here.
+admin.site.register(QUESTION,BookAdmin)
 admin.site.register(HISTORY)
 admin.site.register(DOOR)
 admin.site.register(DOORFAIL)
